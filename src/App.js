49ddb8ad7b646b041774fad52b5a57576f0f2e08
Abhijoy-Mandal <a.mandal@mail.utoronto.ca>
2021-10-27 21:42:55 -0400

    dashboard template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button'
+import Dashboard from './Dashboard';
+
+function App() {
+  
+  return (
+    <Dashboard></Dashboard>
+   
+  );
+}
+
+export default App;
